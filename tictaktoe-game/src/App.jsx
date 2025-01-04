559bf62996f231c62828609fb5fced126443f5ba
Kartikrai-Code <raikartik2004@gmail.com>
2025-01-04 13:56:19 +0530

    add

---
+++ tictaktoe-game/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Tictaktoe from './Components/Tictaktoe/tictaktoe'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+           <Tictaktoe/>
+        </div>
+    </>
+  )
+}
+
+export default App
